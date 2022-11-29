a272047228aad5b328460d02d1940a4b0aa0c8ec
搬砖达人小王git-flow <461864051@qq.com>
2022-11-29 23:53:23 +0800

    feat: debug

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
-import { ConfigProvider, Button } from 'antd';
+import { Button, ConfigProvider, theme } from 'antd';
 import ThemeComp from './components/ThemeComp';
+import DebugIndex from './components/Debug';
 
 const App: React.FC = () => (
 	<ConfigProvider
 		theme={{
-			token: {
-				colorPrimary: '#00b96b',
-			},
+			// algorithm: theme.darkAlgorithm,
+			algorithm: theme.compactAlgorithm,
+			// token: {
+			// 	colorPrimary: '#00b96b',
+			// },
 			components: {
 				Radio: {
-					colorPrimary: 'skyblue',
+					colorPrimary: '#00b96b',
+					// colorPrimary: 'skyblue',
 				},
-				Checkbox: {
-					// TODO 勾选完之后，hover颜色还是绿色？
-					colorPrimary: 'yellow',
-				},
+				// Checkbox: {
+				// 	// TODO 勾选完之后，hover颜色还是绿色？
+				// 	colorPrimary: 'yellow',
+				// },
 			},
 		}}
 	>
 		<ThemeComp />
+		<Button>切换主题</Button>
+		<DebugIndex title='debug学习' />
 	</ConfigProvider>
 );
 
---
+++ src/components/Debug/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function DebugIndex(props: any) {
+	console.log('props: ', props);
+
+	return <div>DebugIndex</div>;
+}
